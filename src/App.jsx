394cadb505d59548d8f92145ff95e19fc41d3cbd
jyotimeena24 <jyoti24meena1234@gmail.com>
2025-03-27 18:12:04 +0530

    codes

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'; // Ensure React is imported
+
+import Navbar from './component/navbar';
+import Footer from './component/footer';
+
+function App() {
+  return (
+    <>
+    <div className='bg-blue-700'>
+      <Navbar />
+      <Footer />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/component/navbar.jsx
@@ -0,0 +1,24 @@
+
+import React from 'react';
+
+function Navbar(){
+
+    return(
+             <>
+             
+             <div>
+                
+
+                <ul className='flex gap-4'>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact us</li>
+                </ul>
+             </div>
+             </>
+    )
+
+
+}
+
+export default Navbar;
