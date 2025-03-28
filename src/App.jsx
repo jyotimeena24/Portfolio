a968bdc59cf041d70f11d5ca67c1aae80f0b4dd5
jyotimeena24 <jyoti24meena1234@gmail.com>
2025-03-28 11:43:12 +0530

    improve the readme.md file

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import React from 'react'; // Ensure React is imported
 
 import Navbar from './component/navbar';
 import Footer from './component/footer';
+import Home from './home';
+import Portfolio from './portfolio';
 
 function App() {
   return (
     <>
     <div >
       <Navbar />
+      <Home />
+      <Portfolio/>
       <Footer />
       </div>
     </>
---
+++ src/home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Home(){
+    return(
+     <>
+     This is home page.
+     
+     </>
+    )
+}
+
+
+export default Home;
